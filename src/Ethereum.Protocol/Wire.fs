e3e97a428baaee7f36e7d6059cad91b6222b827b
kolektiv <andrew@xyncro.com>
2014-04-17 19:15:28 +0100

    protocol changes, and encoding changed to use arrays over lists

---
+++ src/Ethereum.Protocol/Wire.fs
@@ -0,0 +1,113 @@
+﻿module Ethereum.Protocol.Wire
+
+open System
+open System.Net
+open System.Numerics
+open Ethereum.Common
+open Ethereum.Encoding
+open FSharpx
+open FSharpx.Option
+
+// Hello
+
+type Hello =
+    { Protocol: ProtocolVersion
+      Client: string
+      Capabilities: Capabilities
+      Port: int
+      Node: BigInteger option }
+
+    static member encode (x: Hello) =
+        let rlps =
+            [| RLPInt 0x00
+               RLPInt (int x.Protocol)
+               RLPInt 0x00
+               RLPString x.Client
+               RLPInt (int x.Capabilities)
+               RLPInt x.Port |]
+
+        let rlps = Option.getOrElseWith rlps (fun (n: BigInteger) -> 
+            rlps &<! Bytes (n.ToByteArray ())) x.Node
+
+        Array rlps
+
+and ProtocolVersion =
+    | PoC1 = 0x00
+    | PoC2 = 0x01
+    | PoC3 = 0x07
+    | PoC4 = 0x09
+
+and [<Flags>] Capabilities =
+    | PeerDiscovery = 0x01
+    | TransactionRelaying = 0x02
+    | BlockChainQuerying = 0x04
+
+// Disconnect
+
+type Disconnect =
+    { Reason: Reason option }
+
+    static member encode (x: Disconnect) =
+        let rlps = 
+            [| RLPInt 0x01 |]
+
+        let rlps = Option.getOrElseWith rlps (fun r -> 
+            rlps &<! RLPInt (int r)) x.Reason
+
+        Array rlps
+
+and Reason =
+    | Requested = 0x00
+    | TCPError = 0x01
+    | BadProtocol = 0x02
+    | UselessPeer = 0x03
+    | TooManyPeers = 0x04
+    | AlreadyConnected = 0x05
+    | WrongGenesisBlock = 0x06
+    | IncompatibleNetworkProtocol = 0x07
+    | ClientQuitting = 0x08
+    
+// Peers
+
+type Peers =
+    { Peers: Peer list }
+
+    static member encode (x: Peers) =
+        Array (RLPInt 0x011 &>! (x.Peers |> List.map Peer.encode |> Array.ofList))
+
+and Peer =
+    { IP: IPAddress
+      Port: int
+      Node: BigInteger }
+
+    static member encode (x: Peer) =
+        let rlps = 
+            [| Bytes (x.IP.GetAddressBytes ())
+               RLPInt x.Port
+               Bytes (x.Node.ToByteArray ()) |]
+
+        Array rlps
+
+// Protocol
+
+type Protocol =
+    | Hello of Hello
+    | Disconnect of Disconnect
+    | Ping
+    | Pong
+    | GetPeers
+    | Peers of Peers
+
+    static member encode (x: Protocol) =
+        match x with
+        | Hello x -> Hello.encode x
+        | Disconnect x -> Disconnect.encode x
+        | Ping -> Array [| RLPInt 0x02 |]
+        | Pong -> Array [| RLPInt 0x03 |]
+        | GetPeers -> Array [| RLPInt 0x10 |]
+        | Peers x -> Peers.encode x
+
+    static member decode (x: RLP) =
+        match x with
+        | Array [| RLPInt t;  _ |] when t = 0x00 -> None // should check to see how optimized this gets...
+        | _ -> None
--- tests/Ethereum.Encoding.Tests/Encoding.fs
+++ tests/Ethereum.Encoding.Tests/Encoding.fs
@@ -13,8 +13,8 @@ let [<Literal>] lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing eli
 
 // Helpers
 
-let item (x: string) =
-    Item (String.toBytes x)
+let bytes (x: string) =
+    Bytes (String.toBytes x)
 
 let codec (rlp, expected) =
     encode rlp 
@@ -25,34 +25,34 @@ let codec (rlp, expected) =
 
 [<Fact>]
 let ``items of length 0 are encoded as a single prefix byte`` () =
-    codec (Item [], [ 0x80uy ])
+    codec (Bytes [||], [| 0x80uy |])
 
 [<Fact>]
 let ``items of length 1 of value 0x00 are encoded as themselves`` () =
-    codec (Item [ 0x00uy ], [ 0x00uy ])
+    codec (Bytes [| 0x00uy |], [| 0x00uy |])
 
 [<Fact>]
 let ``items of length 1 in the range 0x00 - 0x7f are encoded as themselves`` () =
-    codec (Item [ 0x15uy ], [ 0x15uy ])
+    codec (Bytes [| 0x15uy |], [| 0x15uy |])
 
 [<Fact>]
 let ``items of length < 55 are encoded with a single prefix byte`` () =
-    codec (item dog, 0x83uy :: String.toBytes dog)
+    codec (bytes dog, Array.concat [ [| 0x83uy |]; String.toBytes dog ])
     
 [<Fact>]
 let ``items of length > 55 are encoded with a prefix byte and big endian binary length`` () =
-    codec (item lorem, [ 0xb8uy; 0x38uy ] @ String.toBytes lorem)
+    codec (bytes lorem, Array.concat [ [| 0xb8uy; 0x38uy |]; String.toBytes lorem ])
 
 // List Tests
 
 [<Fact>]
 let ``lists of length 0 are encoded as a single prefix byte`` () =
-    codec (List [], [ 0xc0uy ])
+    codec (Array [||], [| 0xc0uy |])
 
 [<Fact>]
 let ``lists of encoded length < 55 are encoded with a single prefix byte`` () =
-    codec (List [ item cat; item dog ], 0xc8uy :: encode (item cat) @ encode (item dog))
+    codec (Array [| bytes cat; bytes dog |], Array.concat [ [| 0xc8uy |]; encode (bytes cat); encode (bytes dog) ])
 
 [<Fact>]
 let ``lists of encoded length > 55 are encoded with a prefix byte and big endian binary length`` () =
-    codec (List [ item lorem ], [ 0xf8uy; 0x3auy ] @ encode (item lorem))
+    codec (Array [| bytes lorem |], Array.concat [ [| 0xf8uy; 0x3auy |]; encode (bytes lorem) ])
